507330531ca8346a35be8928e70b149d597b2753
Elliot Snow-Kropla <elliot.snowkropla@humi.ca>
2023-02-06 16:11:59 -0400

    pay-336 T3 Re-work

--- lib/taxman/taxman2023/t3.rb
+++ lib/taxman/taxman2023/t3.rb
@@ -3,21 +3,58 @@
 module Taxman2023
   # This is the annual basic federal tax
   class T3
-    attr_reader :a
+    attr_reader :a, :hd
 
-    def initialize(a:)
+    FIRST_THRESHOLD = 53_359_00.to_d
+    SECOND_THRESHOLD = 106_717_00.to_d
+    THIRD_THRESHOLD = 165_430_00.to_d
+    FOURTH_THRESHOLD = 235_675_00.to_d
+
+    def initialize(a:, hd:)
       @a = a
+      @hd = hd
     end
 
     def amount
+      return 0 if a <= 0
+
+      (r * a) - k - k1 - k2 - k3 - k4
     end
 
-    private
+    # rubocop:disable Metrics/MethodLength
+    def r
+      @r ||= if a <= FIRST_THRESHOLD
+               0.150
+             elsif a <= SECOND_THRESHOLD
+               0.205
+             elsif a <= THIRD_THRESHOLD
+               0.260
+             elsif a <= FOURTH_THRESHOLD
+               0.290
+             else
+               0.330
+             end
+    end
+    # rubocop:enable Metrics/MethodLength
 
-    def r; end
-    def k1; end
-    def k2; end
-    def k3; end
-    def k4; end
+    def k
+      0
+    end
+
+    def k1
+      0.15 * Bpaf.new(a: a, hd: hd).amount
+    end
+
+    def k2
+      0
+    end
+
+    def k3
+      0
+    end
+
+    def k4
+      0
+    end
   end
 end
--- spec/taxman/taxman2023/t3_spec.rb
+++ spec/taxman/taxman2023/t3_spec.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.describe Taxman2023::T3 do
-  let(:t3) { described_class.new(a: a).amount }
+  let(:t3) { described_class.new(a: a, hd: hd).amount }
+  let(:hd) { 0 }
 
   context "with no income" do
     let(:a) { 0 }
 
     it "calculates no taxes" do
       expect(t3).to eq 0
+    end
+  end
+
+  context "with $54,000 a year income" do
+    let(:a) { 54_000_00.to_d }
+
+    it "calculates an annulized tax of $5,067.97" do
+      expect(t3).to eq 5_067_97
     end
   end
 end
