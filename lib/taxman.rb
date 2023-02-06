086a0ab7fe09436ebb7a51dc2d41bae51f25e3b6
Elliot Snow-Kropla <elliot.snowkropla@humi.ca>
2023-02-06 14:55:08 -0400

    pay-336 Calculate annualized income

--- lib/taxman.rb
+++ lib/taxman.rb
@@ -3,6 +3,7 @@
 require_relative "taxman/version"
 require_relative "taxman/taxman2023/f5_a"
 require_relative "taxman/taxman2023/f5_b"
+require_relative "taxman/taxman2023/a"
 
 module Taxman
   class Error < StandardError; end
---
+++ lib/taxman/taxman2023/a.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Taxman2023
+  # This calculates the total annual taxable income from the income for the period
+  class A
+    attr_reader :p,
+                :i,
+                :f,
+                :f2,
+                :f5a,
+                :u1,
+                :hd,
+                :f1
+
+    # rubocop:disable Metric/ParameterLists
+    def initialize(
+      p:,
+      i:,
+      f:,
+      f2:,
+      f5a:,
+      u1:,
+      hd:,
+      f1:
+    )
+      @p = p
+      @i = i.to_d
+      @f = f.to_d
+      @f2 = f2.to_d
+      @f5a = f5a.to_d
+      @u1 = u1.to_d
+      @hd = hd.to_d
+      @f1 = f1.to_d
+    end
+    # rubocop:enable Metric/ParameterLists
+
+    def amount
+      a = (p * (i - f - f2 - f5a - u1)) - hd - f1
+
+      return 0 if a <= 0
+
+      a.round(2)
+    end
+  end
+end
