31baee2e13a0c78ec0567e4e24d3d3c60f3c71c8
Elliot Snow-Kropla <elliot.snowkropla@humi.ca>
2023-02-23 13:19:49 -0400

    demo-test Fix problem with Ontario V2

---
+++ spec/integration/taxman2023/demo_test_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+# rubocop:disable RSpec/FilePath, RSpec/MultipleMemoizedHelpers
+RSpec.describe Taxman2023::Calculate do
+  let(:calculate) do
+    described_class.new(
+      period_input: p,
+      year_input: y,
+      td1_input: t,
+      cpp_input: c,
+      ei_input: e
+    ).call
+  end
+
+  let(:p) do
+    Taxman2023::PeriodInput.new(
+      taxable_periodic_income: 3_125,
+      taxable_non_periodic_income: 6_250,
+      province: "on"
+    )
+  end
+
+  let(:y) do
+    Taxman2023::YearInput.new(
+      ytd_bonus: 0,
+      pay_periods: 24,
+      f5b_ytd: 0,
+      employer_ei_multiple: 1.4
+    )
+  end
+
+  let(:t) { Taxman2023::Td1Input.new(federal_personal_amount: 15_000, provincial_personal_amount: 11_865) }
+
+  let(:c) do
+    Taxman2023::CppInput.new(
+      pensionable_income_this_period: 9_375,
+      ytd_contributions: 0,
+      contribution_months_this_year: 12
+    )
+  end
+
+  let(:e) do
+    Taxman2023::EiInput.new(
+      insurable_income_this_period: 9_375,
+      employees_ytd_contributions: 0
+    )
+  end
+
+  it "matches PDOC's federal tax" do
+    expect(calculate[:federal_tax]).to eq 383.94
+  end
+
+  it "matches PDOC's provincial tax" do
+    expect(calculate[:provincial_tax]).to eq 196.64
+  end
+
+  it "matches PDOC's CPP deduction" do
+    expect(calculate[:employee_cpp_contribution]).to eq 549.14
+  end
+
+  it "matches PDOC's EI calculation" do
+    expect(calculate[:employee_ei_contribution]).to be_within(0.1).of 152.82
+  end
+
+  it "matches PDOC's federal bonux tax" do
+    expect(calculate[:federal_tax_on_bonus]).to eq 1_268.64
+  end
+
+  it "matches PDOC's provincial bonus tax" do
+    expect(calculate[:provincial_tax_on_bonus]).to eq 566.25
+  end
+end
+# rubocop:enable RSpec/FilePath, RSpec/MultipleMemoizedHelpers
