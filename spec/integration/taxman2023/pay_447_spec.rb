36201271b834012341ae1f54fbc475664e158810
Elliot Snow-Kropla <elliot.snowkropla@humi.ca>
2023-03-08 10:46:38 -0400

    pay-447 Include L in total output

---
+++ spec/integration/taxman2023/pay_447_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+# rubocop:disable RSpec/FilePath, RSpec/MultipleMemoizedHelpers
+RSpec.describe Taxman2023::Calculate do
+  let(:calculate) do
+    described_class.new(
+      period_input: p,
+      year_input: y,
+      td1_input: t,
+      cpp_input: c,
+      ei_input: e
+    ).call
+  end
+
+  let(:p) do
+    Taxman2023::PeriodInput.new(
+      taxable_periodic_income: 2_400,
+      taxable_non_periodic_income: 0,
+      province: "on"
+    )
+  end
+
+  let(:y) do
+    Taxman2023::YearInput.new(
+      ytd_bonus: 0,
+      pay_periods: 26,
+      f5b_ytd: 0,
+      employer_ei_multiple: 1.4
+    )
+  end
+
+  let(:t) do
+    Taxman2023::Td1Input.new(
+      federal_personal_amount: 15_000,
+      provincial_personal_amount: 11_865,
+      additional_tax_deductions: 150
+    )
+  end
+
+  let(:c) do
+    Taxman2023::CppInput.new(
+      pensionable_income_this_period: 2_400,
+      ytd_contributions: 332.97,
+      contribution_months_this_year: 12
+    )
+  end
+
+  let(:e) do
+    Taxman2023::EiInput.new(
+      insurable_income_this_period: 2_400,
+      employees_ytd_contributions: 97.80
+    )
+  end
+
+  it "matches PDOC's federal tax" do
+    expect(calculate[:federal_tax]).to eq 257.44
+  end
+
+  it "matches PDOC's provincial tax" do
+    expect(calculate[:provincial_tax]).to eq 132.33
+  end
+
+  it "matches PDOC's additional tax" do
+    expect(calculate[:additional_tax]).to eq 150
+  end
+
+  it "matches PDOC's total tax" do
+    expect(calculate[:total_tax]).to eq 539.77
+  end
+
+  it "matches PDOC's CPP deduction" do
+    expect(calculate[:employee_cpp_contribution]).to eq 134.79
+  end
+
+  it "matches PDOC's EI calculation" do
+    expect(calculate[:employee_ei_contribution]).to eq 39.12
+  end
+end
+# rubocop:enable RSpec/FilePath, RSpec/MultipleMemoizedHelpers
