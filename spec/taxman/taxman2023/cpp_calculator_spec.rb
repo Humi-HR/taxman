09c0c5dc5c40c99fcc0fd00f7e47bd22fd5e97b7
Thomas Steven <thomas.steven@humi.ca>
2023-03-20 10:20:46 -0400

    OPS-0 this PR should fail

--- spec/taxman/taxman2023/cpp_calculator_spec.rb
+++ spec/taxman/taxman2023/cpp_calculator_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Taxman2023::CppCalculator do
 
   it "adds the employee cpp contribution to context" do
     expect(cpp_calc.calculate).to match(
-      a_hash_including(employee_cpp_contribution: 427.13.to_d)
+      a_hash_including(employee_cpp_contribution: 99999.00.to_d)
     )
   end
 
