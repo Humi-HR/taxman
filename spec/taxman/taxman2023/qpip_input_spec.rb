314d12cda5239f208bfe9586f48a47b4d4089d0d
Claus Lensbøl <claus.lensboel@humi.ca>
2023-08-17 15:41:14 -0400

    feat: Add input adapter for calculating QPIP

---
+++ spec/taxman/taxman2023/qpip_input_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe Taxman2023::QpipInput do
+  let(:qpip_input) do
+    described_class.new(
+      parental_insurable_income_this_period: s4,
+      ytd_contributions: a6
+    )
+  end
+
+  let(:s4) { 3_500 }
+  let(:a6) { 114 }
+
+  let(:revenue_form) do
+    {
+      qc_s4: 3_500_00.to_d,
+      qc_a6: 114_00.to_d
+    }
+  end
+
+  it "converts its inputs into Revenue Quebec form" do
+    expect(qpip_input.translate).to eq revenue_form
+  end
+end
