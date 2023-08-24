0f285b0a534eb64d48d9b2db187a3e83083de211
Matt Ravenhill <matt.ravenhill@humi.ca>
2023-08-24 13:46:56 -0400

    use province constant and update f5q to use qc_c

--- spec/taxman/taxman2023/f5_q_spec.rb
+++ spec/taxman/taxman2023/f5_q_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe Taxman2023::F5Q do
-  let(:c) { 472_00.to_d }
-  let(:f5) { described_class.new(c: c).amount }
+  let(:qc_c) { 472_00.to_d }
+  let(:f5) { described_class.new(qc_c: qc_c).amount }
 
   it "calculates the portion due to the enhanced cpp rate" do
     expect(f5).to be_within(0.01).of BigDecimal("73_75.00")
