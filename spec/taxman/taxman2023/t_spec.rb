697e4fc4217a865490023aa1bcc15f047a59e135
Claus Lensbøl <claus.lensboel@humi.ca>
2023-08-29 17:14:41 -0400

    feat: Do not add t2 when calculating taxes in QC
    Reference: https://gethumi.atlassian.net/browse/PAY-1081

--- spec/taxman/taxman2023/t_spec.rb
+++ spec/taxman/taxman2023/t_spec.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe Taxman2023::T do
-  let(:t) { described_class.new(t1: 2_000_00, t2: 1_000_00, p: 12, l: 50_00).amount }
+  context "when calculating taxes outside Quebec" do
+    let(:t) { described_class.new(t1: 2_000_00, t2: 1_000_00, p: 12, l: 50_00, province: "ON").amount }
 
-  it "divides the taxes among the periods" do
-    expect(t).to eq 300_00.to_d
+    it "divides the taxes among the periods" do
+      expect(t).to eq 300_00.to_d
+    end
+  end
+
+  context "when calculating taxes for Quebec" do
+    let(:t) { described_class.new(t1: 2_000_00, t2: 1_000_00, p: 12, l: 50_00, province: "QC").amount }
+
+    it "divides the taxes among the periods" do
+      expect(t).to eq 216_66.to_d
+    end
   end
 end
