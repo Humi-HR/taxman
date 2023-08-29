a252e38e30508295b5a39f150496477e39583009
Claus Lensbøl <claus.lensboel@humi.ca>
2023-08-29 17:14:41 -0400

    feat: Do not add t2 when calculating taxes in QC
    Reference: https://gethumi.atlassian.net/browse/PAY-1081

--- lib/taxman/taxman2023/t.rb
+++ lib/taxman/taxman2023/t.rb
@@ -3,21 +3,26 @@
 module Taxman2023
   # Calculates the T factor from the T1 and T2
   class T
-    attr_reader :t1, :t2, :p, :l
+    attr_reader :t1, :t2, :p, :l, :province
 
-    def initialize(t1:, t2:, p:, l:)
+    def initialize(t1:, t2:, p:, l:, province:)
       @t1 = t1
       @t2 = t2
       @p = p
       @l = l
+      @province = province
     end
 
     def self.params
-      %i[t1 t2 p l]
+      %i[t1 t2 p l province]
     end
 
     def amount
-      ((t1 + t2) / p) + l
+      if province == Taxman::QC
+        (t1 / p) + l
+      else
+        ((t1 + t2) / p) + l
+      end
     end
   end
 end
