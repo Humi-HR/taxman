ff7f379d4147049c1bf1d1a1087f0d5b2b972b38
Mark Hogan <mark.hogan@humi.ca>
2023-08-30 15:56:54 -0400

    feat: calculate quebec income tax for the year (Y)

---
+++ lib/taxman/taxman2023/qc_e.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Taxman2023
+  # Value of personal tax credits shown on form TP-1015.3-V
+  class QcE < Factor
+    def self.params
+      %i[qc_e1 qc_e2]
+    end
+    attr_reader(*params)
+
+    def amount
+      (qc_e1 + qc_e2).round
+    end
+  end
+end
