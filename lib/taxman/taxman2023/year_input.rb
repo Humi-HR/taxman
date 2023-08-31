d0b7b6f4fdcc6bdffe320fba1e83e0cc254ea099
Claus Lensbøl <claus.lensboel@humi.ca>
2023-08-31 11:16:38 -0400

    feat: Implement Y1 and Y2 calculations for bonus
    Reference: https://gethumi.atlassian.net/browse/PAY-938

--- lib/taxman/taxman2023/year_input.rb
+++ lib/taxman/taxman2023/year_input.rb
@@ -18,7 +18,8 @@ module Taxman2023
       ytd_deductions_for_employment_income: 0,
       ytd_rsp_deductions: 0,
       ytd_gross_earnings: 0,
-      ytd_csa: 0
+      ytd_csa: 0,
+      ytd_csb: 0
     )
       @b1 = ytd_bonus
       @b1_pensionable = ytd_pensionable_bonus || ytd_bonus
@@ -34,6 +35,7 @@ module Taxman2023
       @qc_g1 = ytd_gross_earnings
       @qc_h1 = ytd_deductions_for_employment_income
       @qc_csa1 = ytd_csa
+      @qc_csb1 = ytd_csb
     end
 
     # rubocop:disable Metrics/AbcSize
@@ -52,7 +54,8 @@ module Taxman2023
         qc_f1: (@qc_f1 * 100).to_d,
         qc_g1: (@qc_g1 * 100).to_d,
         qc_h1: (@qc_h1 * 100).to_d,
-        qc_csa1: (@qc_csa1 * 100).to_d
+        qc_csa1: (@qc_csa1 * 100).to_d,
+        qc_csb1: (@qc_csb1 * 100).to_d
       }
     end
     # rubocop:enable Metrics/AbcSize
---
+++ spec/taxman/taxman2023/qc_csb_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+RSpec.describe Taxman2023::QcCsb do
+  let(:qc_csb) do
+    described_class
+      .new(
+        qc_cs: qc_cs,
+        qc_s3: qc_s3,
+        qc_b2: qc_b2
+      )
+      .amount
+  end
+  let(:qc_cs) { 247_08.3333 }
+  let(:qc_s3) { 25_000_00 }
+  let(:qc_b2) { 20_000_00 }
+
+  it "calculates" do
+    expect(qc_csb.round).to eq(197_67)
+  end
+
+  context "when qc_s3 is zero" do
+    let(:qc_s3) { 0 }
+
+    it "returns zero" do
+      expect(qc_csb).to be_zero
+    end
+  end
+end
