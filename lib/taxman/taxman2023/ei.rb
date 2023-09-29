90101aacd5861235e4a37ac7d35501d45cdfd32e
Claus Lensbøl <claus.lensboel@humi.ca>
2023-09-29 09:01:12 -0400

    feat: Rename optional input for location

--- lib/taxman/taxman2023/ei.rb
+++ lib/taxman/taxman2023/ei.rb
@@ -22,7 +22,7 @@ module Taxman2023
     end
 
     def self.params
-      %i[ie ie_ytd d1 province previously_on_cpp previously_on_qpp]
+      %i[ie ie_ytd d1 province previously_in_non_qc previously_in_qc]
     end
     attr_reader(*params)
 
@@ -35,7 +35,7 @@ module Taxman2023
     end
 
     def ei_calculated
-      if (previously_on_qpp && !quebec?) || (previously_on_cpp && quebec?)
+      if (previously_in_qc && !quebec?) || (previously_in_non_qc && quebec?)
         [[MAXIMUM_INSURABLE - ie_ytd, ie].min * ei_rate, 0].max
       else
         ie * ei_rate
