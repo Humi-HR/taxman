d0a5ad864c2c6d77a96cede822cd030b92abdce9
Mark Hogan <mark.hogan@humi.ca>
2023-11-20 13:46:33 -0500

    update CPP constants

--- lib/taxman/taxman2024/cpp/constants.rb
+++ lib/taxman/taxman2024/cpp/constants.rb
@@ -3,12 +3,12 @@
 module Taxman2024
   module Cpp
     # Bag to hold all of the CPP constants for 2024
-    MAXIMUM_PENSIONABLE = 66_600_00.to_d
-    MAX = 3_754_45.to_d
+    MAXIMUM_PENSIONABLE = 68_500_00.to_d
+    MAX = 3_867_50.to_d
     BASIC_EXEMPTION = 3_500_00.to_d
     RATE = 0.0595.to_d
     LOWER_RATE = 0.0495.to_d
-    MAX_CREDIT = 3_123_45.to_d
+    MAX_CREDIT = 3_217_50.to_d
 
     # Helper method to get the constants as a hash
     class Constants
