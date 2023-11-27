ae04d6e11f8b7e3f02008355af451b7abba20b80
Mark Hogan <mark.hogan@humi.ca>
2023-11-27 15:47:30 -0500

    feat: QC EI, QPP and QPIP rates

--- lib/taxman/taxman2024/ei.rb
+++ lib/taxman/taxman2024/ei.rb
@@ -4,10 +4,10 @@ module Taxman2024
   # Calculates the employee's ei contributions for the period
   class Ei < Factor
     EI_MAX = 1_049_12.to_d
-    QC_EI_MAX = 781_05.to_d
+    QC_EI_MAX = 832_24.to_d
     MAXIMUM_INSURABLE = 63_200_00.to_d
     EMPLOYEE_RATE = 0.0166.to_d
-    QC_EMPLOYEE_RATE = 0.0127.to_d
+    QC_EMPLOYEE_RATE = 0.0132.to_d
     EMPLOYER_MATCHING = 1.4.to_d
 
     # Helper method to get the constants as a hash
