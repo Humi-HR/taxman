a1100417755c76957bc2a8517510f124a58a36ea
Mark Hogan <mark.hogan@humi.ca>
2023-11-27 12:27:08 -0500

    feat: update federal and provincial tax rates and constants

--- lib/taxman/taxman2024/nl/t4.rb
+++ lib/taxman/taxman2024/nl/t4.rb
@@ -5,17 +5,17 @@ module Taxman2024
     # Calculates the annualized provincial tax
     class T4 < T4Generic
       LOWEST_RATE = 0.0870.to_d
-      DEFAULT_TD1 = 10_382_00.to_d
+      DEFAULT_TD1 = 10_818_00.to_d
 
       RATES_AND_CONSTANTS = {
-        41_457_00.to_d => [LOWEST_RATE, 0.0.to_d],
-        82_913_00.to_d => [0.1450, 2_405_00.to_d],
-        148_027_00.to_d => [0.1580, 3_482_00.to_d],
-        207_239_00.to_d => [0.1780, 6_443_00.to_d],
-        264_750_00.to_d => [0.1980, 10_588_00.to_d],
-        529_500_00.to_d => [0.2080, 13_235_00.to_d],
-        1_059_000_00.to_d => [0.2130, 15_883_00.to_d],
-        BigDecimal("Infinity") => [0.2180, 21_178_00.to_d]
+        43_198_00.to_d => [LOWEST_RATE, 0.0.to_d],
+        86_395_00.to_d => [0.1450, 2_505_00.to_d],
+        154_244_00.to_d => [0.1580, 3_629_00.to_d],
+        215_943_00.to_d => [0.1780, 6_713_00.to_d],
+        275_870_00.to_d => [0.1980, 11_032_00.to_d],
+        551_739_00.to_d => [0.2080, 13_791_00.to_d],
+        1_103_478_00.to_d => [0.2130, 16_550_00.to_d],
+        BigDecimal("Infinity") => [0.2180, 22_067_00.to_d]
       }.freeze
     end
   end
--- lib/taxman/taxman2024/nt/t4.rb
+++ lib/taxman/taxman2024/nt/t4.rb
@@ -5,13 +5,13 @@ module Taxman2024
     # Calculates the annualized provincial tax
     class T4 < T4Generic
       LOWEST_RATE = 0.0590.to_d
-      DEFAULT_TD1 = 16_593_00.to_d
+      DEFAULT_TD1 = 17_373_00.to_d
 
       RATES_AND_CONSTANTS = {
-        48_326_00.to_d => [LOWEST_RATE, 0.0.to_d],
-        96_655_00.to_d => [0.0860, 1_305_00.to_d],
-        157_139_00.to_d => [0.1220, 4_784_00.to_d],
-        BigDecimal("Infinity") => [0.1405, 7_691_00.to_d]
+        50_597_00.to_d => [LOWEST_RATE, 0.0.to_d],
+        101_198_00.to_d => [0.0860, 1_366_00.to_d],
+        164_525_00.to_d => [0.1220, 5_009_00.to_d],
+        BigDecimal("Infinity") => [0.1405, 8_053_00.to_d]
       }.freeze
     end
   end
