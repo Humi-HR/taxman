86ac664c8cffc8f5d9438fe8fb25de560c3a65a7
Mark Hogan <mark.hogan@humi.ca>
2023-11-28 11:57:37 -0500

    feat: update provincial S and V1 calculations

--- lib/taxman/taxman2024/bc/t2.rb
+++ lib/taxman/taxman2024/bc/t2.rb
@@ -4,14 +4,14 @@ module Taxman2024
   module Bc
     # Calculates the T2 factor for BC
     class T2 < T2Generic
-      S2 = 521_00.to_d
+      S2 = 547_00.to_d
 
-      LOWER_THRESHOLD = 23_179_00
-      UPPER_THRESHOLD = 37_814_00
+      LOWER_THRESHOLD = 24_338_00
+      UPPER_THRESHOLD = 39_703_00
 
       def s
         return [t4, S2].min if a <= LOWER_THRESHOLD
-        return 0.to_d if a >= UPPER_THRESHOLD
+        return 0.to_d if a > UPPER_THRESHOLD
 
         [t4, s_factor].min
       end
