3517486053d006eb9d7c834714138b310e46ce34
Mark Hogan <mark.hogan@humi.ca>
2023-12-14 10:10:58 -0500

    fix: move test into qc suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  config.exclude_pattern = "spec/integration/taxman2024/{bug,qc,demo,pay,test_case}*"
+  config.exclude_pattern = "spec/integration/taxman2024/{bug,qc,demo,pay}*"
 end
 
 require "debug"
