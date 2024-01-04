9890a2ed985f23b4c072d63ae9bb53165df0634e
Mark Hogan <mark.hogan@humi.ca>
2024-01-04 11:33:10 -0500

    fix: include qc_c2 in qc_cs

--- spec/taxman/taxman2024/qc_cs_spec.rb
+++ spec/taxman/taxman2024/qc_cs_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Taxman2024::QcCs do
   let(:qc_cs) do
     described_class
       .new(
-        qc_c: qc_c
+        qc_c: qc_c,
+        qc_c2: 0
       )
       .amount
   end
@@ -16,5 +17,21 @@ RSpec.describe Taxman2024::QcCs do
 
   it "is less than c" do
     expect(qc_cs).to be < qc_c
+  end
+
+  context "with c2" do
+    let(:qc_c2) { 10 }
+    let(:qc_cs_with_c2) do
+      described_class
+        .new(
+          qc_c: qc_c,
+          qc_c2: qc_c2
+        )
+        .amount
+    end
+
+    it "adds c2" do
+      expect(qc_cs_with_c2).to eq qc_cs + qc_c2
+    end
   end
 end
