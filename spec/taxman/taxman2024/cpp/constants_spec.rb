d200601b54c07ed046f65f564176b2695eb4942c
Claus Lensbøl <claus.lensboel@humi.ca>
2024-03-18 15:57:27 -0400

    feat: Add CPP2 constants

--- spec/taxman/taxman2024/cpp/constants_spec.rb
+++ spec/taxman/taxman2024/cpp/constants_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe Taxman2024::Cpp::Constants do
   it "has the employer matching rate" do
     expect(constants[:cpp_employer_matching]).to eq 1
   end
+
+  it "has the maximum pensionable CPP2 income for a year" do
+    expect(constants[:cpp2_maximum_pensionable]).to eq 73_200
+  end
+
+  it "has the basic CPP2 exemption" do
+    expect(constants[:cpp2_basic_exemption]).to eq 0
+  end
+
+  it "has the employee CPP2 rate" do
+    expect(constants[:cpp2_employee_rate]).to eq 0.04
+  end
+
+  it "has the employer matching CPP2 rate" do
+    expect(constants[:cpp2_employer_matching]).to eq 1
+  end
 end
